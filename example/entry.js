1c68510cca2f305b77e534a1f8e95614818ca74f
Dante <chris.d.federici@gmail.com>
2015-07-23 17:27:16 -0400

    Updated example coverage.

--- example/entry.js
+++ example/entry.js
@@ -9,32 +9,42 @@ var api = new API('//path');
 
 console.log(api.makeRequest());
 
-// Proxy things
-var ProxyAPIFactory = require('proxy!./service/api');
-
+// Mock the dependency
 var mockApiDependency = {
 	doAction: function(path)
 	{
-		return 'I am a mocked depdency. ' + path;
+		return 'I am a mocked dependency. ' + path;
 	}
 };
 
+// Proxy things
+var ProxyAPIFactory = require('proxy!./service/api');
 var ProxyAPI = ProxyAPIFactory({
 	'./apiDependency': mockApiDependency
 });
 
-var mockedApi = new ProxyAPI('//mockpath');
+var mockedApi = new ProxyAPI('//mockPath');
 
 console.log(ProxyAPIFactory);
 console.log(mockedApi.makeRequest());
 
 // Proxy AMD modules
 var PromiseAMDFactory = require('proxy!./service/amdApi');
-
 var ProxyAmdApi = PromiseAMDFactory({
 	'./apiDependency': mockApiDependency
 });
 
-var mockedAmdApi = new ProxyAmdApi('//amdpath');
+var mockedAmdApi = new ProxyAmdApi('//amdPath');
 
 console.log(mockedAmdApi.makeRequest());
+
+
+// AMD with module.exports syntax
+var PromiseAMDDefineFactory = require('proxy!./service/amdDefineApi');
+var ProxyAmdDefineApi = PromiseAMDDefineFactory({
+	'./apiDependency': mockApiDependency
+});
+
+var mockedAmdDefineApi = new ProxyAmdDefineApi('//amdDefinePath');
+
+console.log(mockedAmdDefineApi.makeRequest());
